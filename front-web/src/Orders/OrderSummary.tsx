e4db3bdeecc662fd67f5be4a3886d82f77e2f6b1
Dennys Marcos <kardeenx@gmail.com>
2021-01-10 16:32:44 -0300

    Finished front-web

--- front-web/src/Orders/OrderSummary.tsx
+++ front-web/src/Orders/OrderSummary.tsx
@@ -3,9 +3,10 @@ import { formatPrice } from "./helpers";
 type Props = {
     amount: number;
     totalPrice: number;
+    onSubmit: () => void;
 }
 
-function OrderSummary({amount, totalPrice}: Props) {
+function OrderSummary({amount, totalPrice, onSubmit }: Props) {
     return (
         <div className="order-summary-container">
             <div className="order-summary-content">
@@ -19,7 +20,10 @@ function OrderSummary({amount, totalPrice}: Props) {
                         VALOR TOTAL
                     </span>
                 </div>
-                <button className="order-summary-make-order">
+                <button 
+                    onClick={onSubmit}
+                    className="order-summary-make-order"
+                >
                     FAZER PEDIDO
                 </button>
             </div>
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
-import { fetchProducts } from './api';
+import { fetchProducts, saveOrder } from './api';
 import { checkIsSelected } from './helpers';
 import Footer from '../Footer';
 import OrderLocation from './OrderLocation';
 import OrderSummary from './OrderSummary';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader'
+import { toast } from 'react-toastify';
+
+
 import './styles.css'
 import { OrderLocationData, Product } from './types';
 
@@ -18,11 +21,12 @@ function Orders() {
         return sum + item.price;
     }, 0);
 
-
     useEffect(() => {
         fetchProducts()
             .then(response => setProducts(response.data))
-            .catch(error => console.log(error))
+            .catch(() => {
+              toast.warning("Erro ao listar produtos.");
+            })
     }, []);
 
     const handleSelectProduct = (product: Product) => {
@@ -34,6 +38,23 @@ function Orders() {
         } else {
           setSelectedProducts(previous => [...previous, product]);
         }
+      }
+
+      const handleSubmit = () => {
+        const productsIds = selectedProducts.map(({ id }) => ({ id }));
+        const payload = {
+          ...orderLocation!,
+          products: productsIds
+        }
+      
+        saveOrder(payload)
+        .then((response) => {
+          toast.error(`Pedido enviado com sucesso! Nº ${response.data.id}`);
+          setSelectedProducts([]);
+        })
+          .catch(() => {
+            toast.warning('Erro ao enviar pedido');
+          })
       }
 
     return(
@@ -49,6 +70,7 @@ function Orders() {
                 <OrderSummary 
                     amount={selectedProducts.length}
                     totalPrice={totalPrice}
+                    onSubmit={handleSubmit}
                 />
             </div>
             <Footer />
